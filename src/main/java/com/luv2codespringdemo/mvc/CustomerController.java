e2291e16caba98ca343bbf8411bd0b8f234cfc58
Osama Salih <osamasalih.ca@gmail.com>
2025-03-23 23:15:54 +0300

    Validation using InitBinder

--- src/main/java/com/luv2codespringdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2codespringdemo/mvc/CustomerController.java
@@ -1,15 +1,25 @@
 package com.luv2codespringdemo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
@@ -22,6 +32,7 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer customer,
             BindingResult bindingResult
     )  {
+
         if (bindingResult.hasErrors()) {
             return "customer-form";
         } else {
