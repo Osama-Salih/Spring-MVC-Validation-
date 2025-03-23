0f0d4d0198d01bcddc9b9d4aa22208cf6dae2666
Osama Salih <osamasalih.ca@gmail.com>
2025-03-23 23:49:34 +0300

    Validate a Number Range

--- src/main/java/com/luv2codespringdemo/mvc/Customer.java
+++ src/main/java/com/luv2codespringdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2codespringdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,18 @@ public class Customer {
     @NotNull(message="Is required")
     @Size(min = 1, message="Is required")
     private String lastName;
+
+    @Min(value=0, message="Must be greater then or equal to 0")
+    @Max(value=10, message="Must be lass then or equal to 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getLastName() {
         return lastName;
